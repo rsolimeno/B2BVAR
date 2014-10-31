2f862e5db810b06b7ac489f35b555f90c1c288e4
Bob Solimeno <bsolimeno@collinsinkjet.com>
2014-10-31 08:52:32 -0400

    Modified code to conform to Google's R Style Guide.

--- I-MR_Chart.R
+++ I-MR_Chart.R
@@ -39,22 +39,24 @@ XmRqcc <- function(x, title.X = "Individuals Chart", ylab.X = "Individuals", xla
   
   # Create a QCC object of type xbar.one (qcc's term for 
   # individuals charts) and plot the individuals graph.
-  i.X <- qcc(data=x, type="xbar.one", 
-             title=title.X, 
-             ylab=ylab.X, 
-             xlab=xlab.X, 
-             std.dev=c("MR"), ...)
+  i.X <- qcc(data = x, 
+             type = "xbar.one", 
+             title = title.X, 
+             ylab = ylab.X, 
+             xlab = xlab.X, 
+             std.dev = c("MR"), ...)
   
-  i.adj <- matrix(nrow=length(x)-1, ncol=2)
+  i.adj <- matrix(nrow = length(x) - 1, ncol = 2)
   
-  i.adj[,1] <- x[1:(length(x)-1)]
-  i.adj[,2] <- x[2:length(x)]
+  i.adj[, 1] <- x[1:(length(x) - 1)]
+  i.adj[, 2] <- x[2:length(x)]
   
-  i.mR <- qcc(data=i.adj, type="R", 
-              title=title.mR, 
-              ylab=ylab.mR, 
-              xlab=xlab.mR,
-              std.dev=c("UWAVE-R"), ...)
+  i.mR <- qcc(data = i.adj, 
+              type = "R", 
+              title = title.mR, 
+              ylab = ylab.mR, 
+              xlab = xlab.mR,
+              std.dev = c("UWAVE-R"), ...)
   rval <- list(i.X, i.mR)
   return(rval)
 }
@@ -77,20 +79,21 @@ x <- data$Dev
 obj <- qcc(x, type = "xbar.one")
 
 
-qcc.options("cex.stats"=0.65)
-par(mfrow=c(2,2),
+qcc.options("cex.stats" = 0.65)
+par(mfrow = c(2, 2),
     cex.axis = 0.5,
     cex.lab = 0.75,
-    cex.main=0.95,
-    cex.sub=0.5)
+    cex.main = 0.95,
+    cex.sub = 0.5)
 
 # replace "test" with the TESTID name for the chart titles
-XmRqcc(x, title.X="Individuals Chart - test",
-       ylab.X="Inidviduals",
-       xlab.X="observation",
-       title.mR="Moving Range Chart - test",
-       ylab.mR="Range", xlab.mR="Moving Range",
-       print=TRUE)
+XmRqcc(x, 
+       title.X = "Individuals Chart - test",
+       ylab.X = "Inidviduals",
+       xlab.X = "observation",
+       title.mR = "Moving Range Chart - test",
+       ylab.mR = "Range", xlab.mR="Moving Range",
+       print = TRUE)
 
 
 xdbar <- mean(x)
@@ -104,8 +107,8 @@ speclimits <- c(LSL, USL)
 
 process.capability(obj,
                    speclimits,
-                   target=TARGET,
-                   print=TRUE)
+                   target = TARGET,
+                   print = TRUE)
 
 
 # Check fit to a normal distribution for validity of results
